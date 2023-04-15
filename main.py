cf0069fe3abd3dd7fabb60a6fc75d653b8b1b0dd
kptRobi <kptRobi@gmail.com>
2023-04-15 20:08:24 +0200

    getting images

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import tensorflow as tf
+import numpy as np
+import tensorflow_datasets as tfds
+from matplotlib import pyplot as plt
 
-print("Hello world")
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print(tf.test.gpu_device_name())
+from utils import setup_gpu
+
+print("Hello woraaaald")
+
+#setup
+setup_gpu()
+data_set = tfds.load('fashion_mnist', split='train')
+iterator = data_set.as_numpy_iterator()
+# Setup the subplot formatting
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+# Loop four times and get images
+for idx in range(4):
+    # Grab an image and label
+    sample = iterator.next()
+    # Plot the image using a specific subplot
+    ax[idx].imshow(np.squeeze(sample['image']))
+    # Appending the image label as the plot title
+    ax[idx].title.set_text(sample['label'])
+plt.show()
---
+++ utils.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+def setup_gpu():
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+    print(tf.test.gpu_device_name())
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
